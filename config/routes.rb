6b85941aaeb6a73d9f06d204ebc9eb49563098dd
Jessica <30883219+jessodri@users.noreply.github.com>
2018-04-23 12:25:33 +1000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users
+
+  get '/contact', to: 'pages#contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
